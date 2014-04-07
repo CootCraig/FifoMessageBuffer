73fed570c2f44e9c485053e6d08c9a46b736331b
Craig Anderson <craig@coot.net>
2014-04-07 07:18:30 -0600

    First run at coding. Not tested yet.

--- FifoMessageBuffer.cpp
+++ FifoMessageBuffer.cpp
@@ -17,9 +17,197 @@
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
+#include <stdlib.h>
+
 #include "FifoMessageBuffer.h"
 
-FifoMessageBuffer::FifoMessageBuffer(unsigned short bufSize) {
-  m_bufSize = bufSize;
+// Use bufPtr instead of malloc'ing. m_bufSize will be bufSize-1 to allow for a null sentinel
+FifoMessageBuffer::FifoMessageBuffer(char * bufPtr, short bufSize) {
+  m_freeBufferOnDestructor = false;
+  initForBuffer(bufPtr, bufSize);
+}
+
+FifoMessageBuffer::FifoMessageBuffer(short bufSize) {
+  m_buffer = malloc(bufSize + 1);
+  m_freeBufferOnDestructor = true;
+  initForBuffer(bufPtr, (bufSize + 1));
+}
+
+FifoMessageBuffer::~FifoMessageBuffer() {
+  if (m_freeBufferOnDestructor == true) {
+    free(m_buffer);
+    m_buffer = NULL;
+    m_bufSize = 0;
+    m_freeBufferOnDestructor = false;
+    m_length = 0;
+  }
+}
+
+// Add a char. return 1 if added. return 0 if buffer is already full.
+short FifoMessageBuffer::add(char aChar) {
+  if (m_length >= m_bufSize) { // already full
+    return 0;
+  }
+  else if (aChar == '\0') {
+    return 0; // no NULL characters will be added.
+  } else { // room for another character
+    char * nextPtr = m_firstCharacter + m_length;
+    if (nextPtr >= m_pastRawBuffer) {
+      nextPtr = m_buffer; // wrapped to start
+    }
+    *nextPtr = aChar;
+
+    ++nextPtr; // add sentinel NULL at end of message
+    if (nextPtr >= m_pastRawBuffer) {
+      nextPtr = m_buffer; // wrapped to start
+    }
+    *nextPtr = '\0';
+    return 1;
+  }
+}
+
+// Add a null terminated char string. return the number of characters added.
+short FifoMessageBuffer::add(char * srcString) {
+  short add_count = 0;
+  int srcPos = 0;
+  for ( ; (srcString[srcPos] != '\0'); ++srcPos ) {
+    add_count = add(srcString[srcPos]);
+    if (add_count == 0) {
+      return srcPos;
+    }
+  }
+  return srcPos;
+}
+
+// Add length characters from src.  Terminates if null in src or buffer fills.
+short FifoMessageBuffer::add(char * src, short length) {
+  short add_count = 0;
+  int srcPos = 0;
+  for ( ; srcPos<length; ++srcPos ) {
+    add_count = add(srcString[srcPos]);
+    if (add_count == 0) {
+      return srcPos;
+    }
+  }
+  return srcPos;
+}
+
+// Clear the buffer. Return the number of characters removed.
+short FifoMessageBuffer::clear() {
+  short clear_count = m_length;
+  m_length = 0;
+  m_firstCharacter = NULL;
+  *m_buffer = '\0';
+  return clear_count;
+}
+
+// Clear count characters from the start of the buffer. Return the number of characters removed.
+short FifoMessageBuffer::clear(short count) {
+  if (m_length <= 0) {
+    return 0;
+  }
+  else if (count >= m_length) {
+    return clear();
+  }
+  else { // count < m_length
+    m_firstCharacter += count;
+    if (m_firstCharacter >= m_pastRawBuffer) {
+      m_firstCharacter = m_buffer  + (nextPtr - m_pastRawBuffer);
+    }
+    return count;
+  }
+}
+
+// return the position of the character, -1 if not found.
+short FifoMessageBuffer::find(char aChar) {
+  if (m_length <= 0) {
+    return -1;
+  }
+  char * nextChar = m_firstCharacter;
+  int foundPos = 0;
+  for (; foundPos<m_length; ++foundPos, ++nextChar) {
+    if (nextChar >= m_pastRawBuffer) {
+      nextChar = m_buffer; // wrap to beginning
+    }
+    if (*nextChar == aChar) {
+      return foundPos;
+    }
+  }
+  return -1;
+}
+
+void FifoMessageBuffer::initForBuffer(char * bufPtr, bufSize) {
+  m_bufSize = bufSize - 1;
+  m_buffer = bufPtr;
+  m_pastRawBuffer + (m_bufSize + 1);
+  m_firstCharacter = NULL;
+  m_length = 0;
+}
+
+char * FifoMessageBuffer::insertPtr() {
+  if (m_length >= m_bufSize) {
+    return NULL;
+  } else {
+    char * nextPtr = m_firstCharacter + m_length;
+
+    ++m_length;
+    if (nextPtr >= m_pastRawBuffer) {
+      nextPtr = m_buffer  + (nextPtr - m_pastRawBuffer);
+    }
+    return nextPtr;
+  }
+}
+
+short FifoMessageBuffer::length() {
+  return m_length;
+}
+
+// pop all available characters if possible. dst is NULL terminated. Return the number of characters copied and removed.
+short FifoMessageBuffer::pop(char *dst, short dst_length) {
+  if (dst_length < 1) {
+    return 0;
+  }
+  else if (m_length <= 0) {
+    *dst = '\0';
+    return 0;
+  }
+  else {
+    short transfer_count = (m_length < dst_length) ? m_length : (dst_length-1);
+    for ( short nextPos = 0; nextPos<transfer_count; ++nextPos ) {
+      dst[nextPos] = *m_firstCharacter;
+      ++m_firstCharacter;
+      if (m_firstCharacter >= m_pastRawBuffer) {
+        m_firstCharacter = m_buffer; // wrap to start of ring buffer
+      }
+    }
+    dst[nextPos] = '\0';
+  }
+  return transfer_count;
+}
+
+// pop dst_length characters if possible. dst is NULL terminated. Return the number of characters copied and removed.
+short pop(short length, char *dst, short dst_length) {
+  if (dst_length < 1) {
+    return 0;
+  }
+  else if (m_length <= 0) {
+    *dst = '\0';
+    return 0;
+  }
+  else {
+    short transfer_count = (length < dst_length) ? length : (dst_length-1);
+    if (transfer_count > m_length) {
+      transfer_count = m_length;
+    }
+    for ( short nextPos = 0; nextPos<transfer_count; ++nextPos ) {
+      dst[nextPos] = *m_firstCharacter;
+      ++m_firstCharacter;
+      if (m_firstCharacter >= m_pastRawBuffer) {
+        m_firstCharacter = m_buffer; // wrap to start of ring buffer
+      }
+    }
+    dst[nextPos] = '\0';
+  }
+  return transfer_count;
 }
 
--- FifoMessageBuffer.h
+++ FifoMessageBuffer.h
@@ -20,17 +20,36 @@
 #ifndef FIFOMESSAGEBUFFER_H
 #define FIFOMESSAGEBUFFER_H
 
-Class FifoMessageBuffer {
+class FifoMessageBuffer {
   private:
-    static const short defaultBufSize = 256;
+    static const short defaultBufSize = 255;
   public:
     FifoMessageBuffer() : FifoMessageBuffer(defaultBufSize) { };
-    FifoMessageBuffer(unsigned short bufSize);
+    FifoMessageBuffer(char * bufPtr, short bufSize); // Use this buffer instead of malloc'ing. length with be bufSize-1 to allow for a null sentinel
+    FifoMessageBuffer(short bufSize);
     ~FifoMessageBuffer();
 
-    short bufSize() { return m_bufSize };
+    short add(char aChar); // Add a char. return 1 if added. return 0 if buffer is already full.
+    short add(char * srcString); // Add a null terminated char string. return the number of characters added.
+    short add(char * src, short length); // Add length characters from src.  Terminates if null in src or buffer fills.
+    short bufSize() { return m_bufSize; } // maximum message length. One less than actual buffer size.
+    short clear(); // Clear the buffer. Return the number of characters removed.
+    short clear(short count); // Clear count characters from the start of the buffer. Return the number of characters removed.
+    short find(char src); // return the position of the character, -1 if not found.
+    short length();
+    short pop(char *dst, short dst_length); // pop all available characters if possible. dst is left null terminated. Return the number of characters copied and removed.
+    short pop(short length, char *dst, short dst_length); // pop length characters if possible. dst is left null terminated. Return the number of characters copied and removed.
+
   private:
-    short m_bufSize;
+    char * m_buffer; // 1 more than m_bufSize so there is a null sentinel character
+    short m_bufSize; // Maximum message length. 1 less than actual buffer length.
+    char * m_firstCharacter; // NULL with m_length==0 when no characters available.
+    bool m_freeBufferOnDestructor;
+    short m_length;
+    char * m_pastRawBuffer;
+
+    void initForBuffer(char * bufPtr, bufSize);
+    char * insertPtr();
 };
 
 #endif // FIFOMESSAGEBUFFER_H
